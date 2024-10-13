bdf6676cc06072d5f94b8caf9b8208e1180329b2
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-13 18:20:35 -0400

    Sanitize html and javascript

--- src/api/sanitization_utils.py
+++ src/api/sanitization_utils.py
@@ -1,4 +1,13 @@
-def validate_input_type(input_text):
+import re
+
+
+def validate_input_type(text):
     if not isinstance(input_text, str):
         raise ValueError("Invalid input type. Expected a string.")
+    return input_text
+
+def sanitize_html_and_js(input_text):
+    # Remove all <script> tags and all other HTML tags
+    input_text = re.sub(r'<script.*?>.*?</script>', '', input_text, flags=re.IGNORECASE)
+    input_text = re.sub(r'<.*?>', '', input_text)
     return input_text
