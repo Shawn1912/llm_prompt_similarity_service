6e7864756cd7a230524a88e37683c814f972e59f
Shawn Louis <shawnlouis2000@gmail.com>
2024-10-13 19:22:44 -0400

    Renamed file and removed unused file

--- tests/test_sanitization.py
+++ tests/test_sanitization.py
@@ -1,5 +1,16 @@
 import unittest
-from src.api.sanitization import sanitize_output
+from src.api.sanitization import sanitize_input, sanitize_output
+
+class TestInputSanitization(unittest.TestCase):
+    def test_sanitize_input(self):
+        input_prompt = "<b>Test</b> with some extra text."
+        sanitized = sanitize_input(input_prompt)
+        self.assertEqual(sanitized, "Test with some extra text.")
+
+    def test_length_limit(self):
+        input_prompt = "a" * 600
+        sanitized = sanitize_input(input_prompt)
+        self.assertEqual(len(sanitized), 500)
 
 class TestOutputSanitization(unittest.TestCase):
     def test_sanitize_output_removes_disallowed_words(self):
